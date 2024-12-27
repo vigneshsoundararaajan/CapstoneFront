09562fbb9b7816cd8e80e83e907242858fe10578
Vignesh Soundararajan <vigneshsviolet@gmail.com>
2024-12-27 22:28:24 +0530

    Version 2.0

--- src/app/order/order-list/order-list.component.ts
+++ src/app/order/order-list/order-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { Order } from '../order.model';
+import { ToastService } from '../../shared/toast.service';
 
 @Component({
   selector: 'app-order-list',
-  imports: [],
   templateUrl: './order-list.component.html',
-  styleUrl: './order-list.component.css'
+  styleUrls: ['./order-list.component.css']
 })
-export class OrderListComponent {
+export class OrderListComponent implements OnInit {
+  orders: Order[] = [];
 
+  constructor(private orderService: OrderService, private toastService: ToastService) { }
+
+  ngOnInit(): void {
+    this.orderService.getOrders().subscribe(data => {
+      this.orders = data;
+    });
+  }
+
+  removeOrder(order: Order) {
+    this.orderService.deleteOrder(order.id).subscribe(() => {
+      this.orders = this.orders.filter(o => o.id !== order.id);
+      this.toastService.show(`Order for ${order.productName} removed!`);
+    });
+  }
 }
